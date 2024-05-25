d99d78caf29b9aef7c02bdc56f74a55702aad278
freshposh-code <atomisefarouk919@gmail.com>
2024-05-25 17:44:06 +0100

    update on main

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import * as Sentry from "@sentry/react";
+// import * as Sentry from "@sentry/react";
 
-Sentry.init({
+// Sentry.init({
 
-  dsn: "https://23a5a93d4eef0f99e985773d8134c29d@o4507308116017152.ingest.us.sentry.io/4507317468790784",
-  integrations: [
-    new Sentry.BrowserTracing(),
-    new Sentry.Replay(),
-  ],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+//   dsn: "https://23a5a93d4eef0f99e985773d8134c29d@o4507308116017152.ingest.us.sentry.io/4507317468790784",
+//   integrations: [
+//     new Sentry.BrowserTracing(),
+//     new Sentry.Replay(),
+//   ],
+//   tracesSampleRate: 1.0,
+//   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1.0,
+// });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
