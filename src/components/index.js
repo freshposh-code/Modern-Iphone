248f96f720c0a70a735ee7f3717a86f96d606b7c
freshposh-code <atomisefarouk919@gmail.com>
2024-05-25 01:06:20 +0100

    Add export for components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from './Navabr'
+export { default as Hero } from './Hero'
+export { default as Highlight } from './Highlights'
+export { default as Model } from './Model'
+export { default as Features } from './Features'
+export { default as HowItWorks } from './HowItWorks'
+export { default as Footer } from './Footer'
