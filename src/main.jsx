c7e3fe186ba559ffc5de76dedd9afdb44c9e6963
freshposh-code <atomisefarouk919@gmail.com>
2024-05-25 16:01:08 +0100

    commit changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-//...
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://d674932a77e6d9b9ced1190d70fd4691@o4506876178464768.ingest.us.sentry.io/4506876181151744",
   integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.metrics.metricsAggregatorIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-    }),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
+    new Sentry.BrowserTracing(),
+    new Sentry.Replay(),
   ],
   tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
@@ -28,5 +19,5 @@ Sentry.init({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
